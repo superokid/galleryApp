ac9e75838d4f0dc60670e3c3102fd594bf520996
Kelvin Hong <kelvinok52@gmail.com>
2021-10-31 09:53:44 +0700

    tabbar

---
+++ src/pages/Bookmark.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text } from '../components';
+
+interface Props {}
+
+const Bookmark = (props: Props) => {
+  return <Text>bookmark</Text>;
+};
+
+export default Bookmark;
--- src/routes/RouterWrapper.tsx
+++ src/routes/RouterWrapper.tsx
@@ -1,18 +1,40 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-import routes from './routes';
+import Home from '../pages/Home';
+import ArtDetail from '../pages/ArtDetail';
+import Bookmark from '../pages/Bookmark';
 
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const TabComponent = () => (
+  <Tab.Navigator>
+    <Tab.Screen name="Home" component={Home} options={{ headerShown: false }} />
+    <Tab.Screen
+      name="Bookmark"
+      component={Bookmark}
+      options={{ headerShown: false }}
+    />
+  </Tab.Navigator>
+);
 
 const RouterWrapper = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        {routes.map(route => {
-          return <Stack.Screen {...route} key={route.name} />;
-        })}
+        <Tab.Screen
+          name="HomeTab"
+          component={TabComponent}
+          options={{ headerShown: false }}
+        />
+        <Tab.Screen
+          name="ArtDetail"
+          component={ArtDetail}
+          options={{ headerShown: false }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
